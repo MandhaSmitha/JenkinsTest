b265091d1cf560f8c75874a8366d76198269b147
Smitha <mandha.reddy@emirates.com>
2017-06-02 23:56:08 +0400

    Initial commit
    Initial commit

---
+++ JenkinsTest/JenkinsTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JenkinsTest
+//
+//  Created by Mandha Reddy on 6/2/17.
+//  Copyright © 2017 Mandha Reddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
